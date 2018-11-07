b9be8dcf8b83105d632e8e11a42a72e430626eb5
alex hicks <alexhicks@live.ca>
2018-11-07 07:58:44 -0500

    Map Testing
    - Added in a testing map which will be used for first ONLINE game play
    - Added a RnD folder for scenes for people that need to test things locally such as the player controller
    - Brought over GameobjectSceneTag which gives a visual circle in editor to gameobjects that are just using the transform component such as spawns

---
+++ RPG-Unity2DChallenge/Assets/Code/Gameplay/MapData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Project.Gameplay {
+    public class MapData : MonoBehaviour {
+
+        [SerializeField]
+        private string mapName = "";
+        [SerializeField]
+        private int realmID = -1;
+        [SerializeField]
+        private Transform startingPosition;
+
+        /*[Header("Extra Data")]
+        [SerializeField]
+        private SpawnFromRealmData[] extraSpawns;*/
+
+		public int GetRealmID() {
+            return realmID;
+        }
+
+        public Vector3 GetStartingPosition(int OldRealm) {
+
+            /*if (extraSpawns != null) {
+                if (extraSpawns.Any(x => x.FromRealm == OldRealm)) {
+                    return extraSpawns.First(x => x.FromRealm == OldRealm).Spawn.position;
+                }
+            }*/
+
+            return startingPosition.position;
+        }
+	}
+
+    /*[Serializable]
+    public class SpawnFromRealmData {
+        public int FromRealm;
+        public Transform Spawn;
+    }*/
+}
---
+++ RPG-Unity2DChallenge/Assets/Code/Utility/GameobjectSceneTag.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Project.Utility {
+    public class GameobjectSceneTag : MonoBehaviour {
+
+        [SerializeField]
+        private Color debugColour = Color.cyan;
+
+		public void Start () {
+            Destroy(this);
+		}
+
+        public void OnDrawGizmos() {
+            Gizmos.color = debugColour;
+            Gizmos.DrawIcon(transform.position, "SpawnIcon.png", true);
+            //Gizmos.DrawSphere(transform.position, 0.5f);
+
+#if UNITY_EDITOR
+            Handles.color = debugColour;
+            Handles.DrawWireDisc(transform.position, Vector3.forward, 0.25f);
+#endif
+
+        }
+    }
+}
