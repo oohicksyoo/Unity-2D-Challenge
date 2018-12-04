45984fb9675b0f9caf8bcf3d94b93dab5b099cf6
alex hicks <alexhicks@live.ca>
2018-12-04 08:06:03 -0500

    Parallax
    - Added tweaks to the jungle map
    - Added base parallaxing to the background base on your character

---
+++ RPG-Unity2DChallenge/Assets/Code/Gameplay/Parallax.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project.Gameplay {
+    public class Parallax : MonoBehaviour {
+
+        [SerializeField]
+        private Transform[] backgrounds;
+        public float smoothing = 1.0f;
+
+        private float[] parallaxScales;
+        private Transform cam;
+        private Vector3 previousCameraPosition;
+
+
+        public void Awake() {
+            cam = Camera.main.transform;
+        }
+
+        public void Start () {
+            previousCameraPosition = cam.position;
+
+            parallaxScales = new float[backgrounds.Length];
+            for (int i = 0; i < backgrounds.Length; i++) {
+                parallaxScales[i] = backgrounds[i].position.z * -1;
+            }
+		}
+		
+		public void Update () {
+            for (int i = 0; i < backgrounds.Length; i++) {
+                float parallax = (previousCameraPosition.x - cam.position.x) * parallaxScales[i];
+                float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+                Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+                backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+            }
+
+            previousCameraPosition = cam.position;
+		}
+	}
+}
